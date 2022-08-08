99fd770916d2c5886d2cb2cba211ff29b077d49c
reza2boyce <reza2boyce@gmail.com>
2022-08-08 15:43:59 +0430

    learn style and add image

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Button,
+  Image,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed!");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Test text</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.textRight}>فریبا افراز</Text>
+      <Image
+        blurRadius={0}
+        fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
+      <Button title="Press Me" onPress={() => Alert.alert("خوش آمدید هرمز")} />
+      <div>this is a test text for this project</div>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +33,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    rowGap: "1rem",
+  },
+  textRight: {
+    textAlign: "right",
+    backgroundColor: "red",
+    color: "white",
+    padding: "1rem",
+    borderRadius: "1rem",
+  },
+  bottomNavbar: {
+    position: "fixed",
+    bottom: "0",
+    left: "0",
+    width: "100%",
+    height: "60px",
+    backgroundColor: "blue",
   },
 });
